a17ac860d38a9a3fd13bff642c18539e5bef1f4b
Ивлев Вадим <ivlev@rg.ru>
2019-05-17 14:34:35 +0300

    fix: html

--- templates/index.js
+++ templates/index.js
@@ -1,88 +1,32 @@
 
-
-var b64array =[]
-
-function inputFileChangeHandler(event) {
-    clear()
-    var files = event.target.files
-    for (var file of files) {
-        var reader = new FileReader()
-        reader.fileName = file.name
-        reader.addEventListener("load", function (e) {
-            var res = e.target.result
-            b64 = res.substr(res.search("base64,") + "base64,".length)
-            b64array.push({ fileName: e.target.fileName, base64: b64 })
-            addImage(res)
-        }, false)
-        reader.readAsDataURL(file)
-    }
-}
-
-function getDataToUpload(b64array) {
-    var data = {
-        query : '',
-        variables: '{}'
-    }
-    var query = ""
-    b64array.forEach(function (f, i) {
-        query += createOneQuery(i, f.fileName, f.base64)
-    });
-    data.query = "mutation {\n"+query+"\n}\n"
-    return data
-}
-
-function SEND(){
-    data = getDataToUpload(b64array)
-    // console.log(data.query)
-    $.post('/graphql', data, function(response) {$('#result0').text(JSON.stringify(response, null,'  '));}, 'json'  )
-    tweakUI(data)
-}
-
-
-
-
-
-function tweakUI(data){
-    $('#form0 textarea[name="query"]').hide()
-    $('#showHideButton').show()
-    document.querySelector('#form0 textarea[name="query"]').value = data.query
-}
-
-
-
-
-
-function addImage(src) {
-    $('#img_container').append(`<img style="max-width: 150px;" src="${src}">`)
-}
-
-
-function createOneQuery(i, fileName, b64string) {
-    return `
-    new${i}: create_image( post_id: ${ document.querySelector('#formA input[name="post_id"]').getAttribute('value') }, 
-        source: "${ document.querySelector('#formA input[name="source"]').getAttribute('value') }", 
-        filename: "${fileName}",
-        base64: "${b64string}"
-    ) 
-    {   
-        id 
-        post_id  
-        source 
-		thumbs {
-				type
-				filepath
-				height
-				width
-			}
-        filepath  
-    }
-    `
-}
-
-function clear(){
-    $('#img_container').html('')
-    b64array=[]
-}
+            // подравниваем высоты textarea
+            function adjustTextarea() {
+                $("textarea").each(function (textarea) {
+                    $(this).height($(this)[0].scrollHeight);
+                });
+            }
+    
+            // Accordion
+            function accordionize(){
+                $( "ol" ).accordion({
+                    header: "li > div.comment",
+                    active: false,
+                    heightStyle: "content",
+                    collapsible: true,
+                    animate: 50,
+                    classes: {
+                        "ui-accordion-header": "header",
+                        "ui-accordion-header-collapsed": "header-collapsed",
+                        "ui-accordion-content": "content"
+                    }
+                })
+    
+                
+            }
+    
+            adjustTextarea()
+            accordionize()
+            // setTimeout('accordionize()',3000)
 
 
 
