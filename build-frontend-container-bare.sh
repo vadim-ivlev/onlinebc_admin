a81ee40e2c33ea7a9c180d4f8939723b457010bf
Ивлев Вадим <ivlev@rg.ru>
2019-06-11 15:59:54 +0300

    fix: Build inside docker container. Closed #23.
    fix: Exact go module versioning. Closed #24.

--- build-frontend-container-bare.sh
+++ build-frontend-container-bare.sh
@@ -7,15 +7,18 @@ docker-compose down
 # удаляем файлы бд, и чистим загрузки
 sudo rm -rf pgdata uploads/* uploads_temp/*
 
+# компилируем. линкуем статически под линукс
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a  .
+
 
 # build a docker image 
-docker build -t rgru/onlinebc_admin:latest -f Dockerfile-frontend . 
+docker build -t rgru/onlinebc_admin:bare -f Dockerfile-frontend-bare . 
 
 # push the docker image 
 docker login
-docker push rgru/onlinebc_admin:latest
+docker push rgru/onlinebc_admin:bare
 
 
 # копируем docker-compose-frontend.yml и 
-cp docker-compose-frontend.yml ../onlinebc/docker-compose.yml
+cp docker-compose-frontend-bare.yml ../onlinebc/docker-compose.yml
 cp readme-frontend.md ../onlinebc/readme.md
---
+++ go.mod
@@ -0,0 +1,19 @@
+module onlinebc_admin
+
+go 1.12
+
+require (
+	github.com/disintegration/imaging v1.6.0
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/graphql-go/graphql v0.7.8
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.1.1
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/ugorji/go v1.1.5-pre // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/yaml.v2 v2.2.2
+)
